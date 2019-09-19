7adac442a7aa22cee0db385fdacaaf0cacfbde35
Aguma Chales Jalameso <chalesaguma@live.com>
2019-09-19 17:43:01 +0300

    created citieis listing for search

--- search/settings.py
+++ search/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
-    'cities.apps.CitiesConfig'
+    'citieis.apps.CitieisConfig'
 ]
 
 MIDDLEWARE = [
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'search.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
